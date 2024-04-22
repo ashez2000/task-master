7a79504e12378acc532600814cc46fa6228044b7
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-04-22 13:24:18 +0530

    Add project invite route

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -1,4 +1,5 @@
 import db from '../utils/prisma.js'
+import * as projectRepo from '../repository/project.js'
 import { AppError } from '../utils/error.js'
 
 /**
@@ -28,14 +29,48 @@ export const create = async (req, res) => {
  */
 export const findById = async (req, res) => {
   const id = req.params.id
+  const project = await findProjectById(id)
+  res.status(200).json(project)
+}
 
-  const project = await db.project.findUnique({
-    where: { id },
+/**
+ * Invite user to project. Only project owner can invite users
+ * @route POST /api/projects/:id/invite
+ */
+export const invite = async (req, res) => {
+  const curUserId = req.user.id
+  const projectId = req.params.id
+  const { userId: inviteId } = req.body
+
+  const project = await findProjectById(projectId)
+  // Check ownership
+  isOwner(project, curUserId)
+
+  const invite = await db.projectInvite.create({
+    data: {
+      projectId,
+      userId: inviteId,
+    },
   })
 
+  res.status(201).json(invite)
+}
+
+// #### Helpers ####
+
+/** Finds project by id, throws Not Found on null*/
+const findProjectById = async (id) => {
+  const project = await projectRepo.findById(id)
   if (!project) {
     throw new AppError('Project not found!', 404)
   }
+  return project
+}
 
-  res.status(200).json(project)
+/** Checks for project ownership, else throws Forbidden error */
+const isOwner = (project, curUserId) => {
+  if (project.userId !== userId) {
+    console.debug('User is unautorized to invite for this project')
+    throw new AppError('Forbidden', 403)
+  }
 }
--- src/routes/project.js
+++ src/routes/project.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
+import { protect } from '../middlewares/auth.js'
 import * as project from '../controllers/project.js'
 
 const router = Router()
 
-router.post('/', project.create)
+router.post('/', protect, project.create)
 router.get('/:id', project.findById)
+router.post('/:id/invite', protect, project.invite)
 
 export default router
